3dfb872595f0c6cff2c409caa75160a89d5e8280
William G Hatch <william@hatch.uno>
2020-06-19 15:06:31 -0600

    work on implementation

--- main.rkt
+++ main.rkt
@@ -1,101 +1,201 @@
 #lang racket/base
 
-(provide
- ;; TODO - contracts.  Note that the contract for extenorc field names should be (and/c symbol? symbol-interned?)
- empty-extenor
- ;; TODO - a basic extenor with some good properties -- eg. prop:custom-write
- add-extenorc
- get-extenor-field
- set-extenor-field
- get-extenor-keys
- get-extenor-struct-type-properties
- define-extenorc
- make-extenorc
- make-prop-extenorc
- extenor?
- extenorc?
- ; remove-extenorc
- ; merge-extenors
- )
+(require racket/contract)
+(define (isym? x) (and (symbol? x) (symbol-interned? x)))
 
-;; TODO - document the structure of these
+(provide
+ (contract-out
+  [extenor? (-> any/c any/c)]
+  [empty-extenor extenor?]
+  [add-extenorc (->* (extenor? extenorc?) () #:rest (listof any/c) extenor?)]
+  [get-extenor-field (->* (extenor? isym?) (any/c) any/c)]
+  [set-extenor-field (-> extenor? isym? any/c any/c)]
+  [get-extenor-keys (-> extenor? (listof isym?))]
+  [get-extenor-struct-type-properties (-> extenor? (listof structure-type-property?))]
+  [remove-extenorc (-> extenor? extenorc? extenor?)]
+  ; TODO - merge-extenors
+
+  [make-extenorc (->* ()
+                      (#:name (or/c not isym?)
+                       #:guard (or/c not procedure?)
+                       #:properties (hash/c structure-type-property? any/c))
+                      #:rest (listof (cons/c (or/c 'hidden 'visible)
+                                             isym?))
+                      (list/c extenorc?
+                              (-> any/c any/c)
+                              (listof (-> extenor? any/c))
+                              (listof (-> extenor? any/c extenor?))))]
+  [extenorc? (-> any/c any/c)]
+  [extenorc-name (-> extenorc? any/c)]
+  [get-extenorc-struct-type-properties
+   (-> extenorc? (listof structure-type-property?))]
+  [make-prop-extenorc (-> structure-type-property? extenorc?)]
+  )
+ define-extenorc
+ ;; TODO - a basic extenor with some good properties -- eg. prop:custom-write
+ )
 
 ;; An extenor has:
 ;; * The struct-type constructor for its type (for quickly setting fields without
 ;;   defining a new struct-type with all the properties).
-;; * A hash table from extenorc to the extenorc's index in the content vector.
-;;   If there are no fields in the extenorc, index is #f.
-;; * A content vector, which itself contains:
-;;   - For an extenorc with one field, it just has that field's value directly.
-;;   - For an extenorc with more than one field, a vector for those fields.
-;;   - Nothing for an extenorc with no fields (eg. maybe it has just properties).
-(struct extenor (struct-type-constructor extenorc-table content-vector))
+;; * A hash table from extenorc to the extenorc's data.
+;;   -  If there is one field, the hash table value for the extenorc is that field.
+;;   -  If there are multiple fields, the hash table holds a list of the fields.
+;; Extenors can be extended with more extenorcs.  If an extenorc adds a new property,
+;; the extended extenor must use a new subtype of the base extenor struct that includes
+;; the property.  If the extenorc does not add a new property, then the current struct
+;; constructor can be re-used.
+(struct extenor (struct-type-constructor extenorc-table))
 
-(struct extenorc (field-spec-list property-list guard))
+;; extenorcs have:
+;; * A name, which is an interned symbol or #f. (TODO - this may change)
+;; * A field spec list, where each field spec is (cons visibility field-name)
+;;   where visibility is either 'hidden or 'visible and field-name is an
+;;   interned symbol.
+;; * A property alist, mapping struct-type-properties to the relevant values.
+;; * a guard, which is either #f or a function that accepts one argument for
+;;   each field and returns a list of the same number of fields.  It can
+;;   either raise an exception to block construction with bad data or it
+;;   can transform the data for construction.
+(struct extenorc (name field-spec-list property-alist guard))
 
-(define empty-extenor (extenor (hasheq) (vector-immutable)))
+(define empty-extenor (extenor extenor (hasheq)))
+
+(define (build-extenor-constructor prop-alist)
+  (define-values (type constructor predicate accessor mutator)
+    (make-struct-type (gensym) struct:etxnor 0 0 0 prop-alist))
+  constructor)
 
 (define (add-extenorc an-extenor an-extenorc . field-vals)
   ;; Check whether the extenorc is already there.
-  (when (hash-contains-key? an-extenor an-extenorc)
+  (when (hash-contains-key? (extenor-extenorc-table an-extenor) an-extenorc)
     (error 'add-extenorc "the extenor already contains the extenorc: ~v" an-extenorc))
   ;; Check whether any visible fields of the extenorc conflict with existing
   ;; visible fields in the extenor.
-  (void 'TODO)
+  (define field-specs (extenorc-field-spec-list an-extenorc))
+  (define field-conflict
+    (for/or ([field-spec field-specs])
+      (and (eq? (car field-spec) 'visible)
+           (not (eq? opaque-flag
+                     (get-extenor-field an-extenor
+                                        (cdr field-spec)
+                                        opaque-flag)))
+           (cdr field-spec))))
+  (when field-conflict
+    (error 'add-extenorc "extenor already contains visible field: ~v"
+           field-conflict))
   ;; Check whether any properties of the extenorc conflict with existing properties
   ;; on the extenor.
-  (void 'TODO)
+  (define new-props-alist (extenorc-prop-alist an-extenorc))
+  (define old-props-alist
+    (and (not (null? new-props-alist))
+         (for/fold ([props '()])
+                   ([extenorc (hash-keys
+                               (extenor-extenorc-table an-extenor))])
+           (append (if (symbol? extenorc)
+                       '()
+                       (extenorc-property-alist extenorc))
+                   props))))
+  (define prop-conflict
+    (for/or ([prop (map car (extenorc-prop-alist an-extenorc))])
+      (and (assq prop old-props)
+           prop)))
+  (when prop-conflict
+    (error 'add-extenorc "extenor already contains struct-type-property: ~v"
+           prop-conflict))
   ;; If there are new properties, we need to make a new struct-type
   ;; that is a subtype of extenor that has all struct-type-properties
   ;; of all extenorcs.
-  (void 'TODO)
-  ;; If there are new fields, we need to extend the content-vector.
-  (void 'TODO)
-  ;; For all extenorcs we need to extend the extenorc table.
-  (void 'TODO)
-  (error 'TODO-implement-add-extenorc))
+  (define new-constructor
+    (if (null? new-props-alist)
+        old-constructor
+        (build-extenor-constructor (append new-props-alist old-props-alist))))
 
-(define opaque-flag (gensym))
+  ;; We need to add any new fields, applying guards as necessary.
+  (define field-vals-l (length field-vals))
+  (define field-specs-l (length field-specs))
+  (when (not (eq? field-vals-l
+                  field-specs-l))
+    (error 'add-extenorc "Improper number of field values.  Expected ~a, received ~a"
+           field-specs-l
+           field-vals-l))
+  (define guarded-field-vals
+    (if (extenorc-guard extenorc)
+        (apply extenorc-guard field-vals)
+        field-vals))
+  (when (and (not (eq? field-vals guarded-field-vals))
+             (not (eq? field-vals-l (length guarded-field-vals))))
+    (error 'add-extenorc
+           "Guard procedure returned the wrong number of fields for extenorc: ~v"
+           (extenorc-name an-extenorc)))
+  (define new-contents
+    (cond [(eq? 0 field-vals-l) '()]
+          [(eq? 1 field-vals-l) (car guarded-field-vals)]
+          [else guarded-field-vals]))
+  (define new-table (hash-set (extenor-extenorc-table an-extenor)
+                              an-extenorc
+                              new-contents))
+  (new-constructor new-constructor new-table))
 
-(define (do-extenor-walk set? an-extenor field-symbol new-value-or-fallback)
+(define (remove-extenorc an-extenor an-extenorc)
+  (define extenor-constructor (extenor-struct-type-constructor))
+  (define new-table (hash-remove (extenor-extenorc-table an-extenor) an-extenorc))
+  (if (null? (extenorc-property-alist an-extenorc))
+      (extenor-constructor
+       extenor-constructor
+       (hash-remove (extenor-extenorc-table an-extenor) an-extenorc))
+      (let* ([prop-alist (for/fold ([props '()])
+                                   ([extenorc (hash-keys new-table)])
+                           (if (symbol? extenorc)
+                               props
+                               (append (extenorc-property-alist extenorc)
+                                       props)))]
+             [new-constructor (build-extenor-constructor prop-alist)])
+        (new-constructor new-constructor new-table))))
+
+
+(define (do-extenor-walk/break set? an-extenor field-symbol new-value-or-fallback)
   (define result-tentative
     (for/fold ([result opaque-flag])
-              ([(extenorc index) (in-hash (extenor-extenorc-table an-extenor))])
+              ([(extenorc contents) (in-hash (extenor-extenorc-table an-extenor))])
       #:break (not (eq? result opaque-flag))
       (if (eq? field-symbol extenorc)
           (if set?
-              (vector-set (extenor-content-vector an-extenor)
-                          index new-value-or-fallback)
-              (vector-ref (extenor-content-vector an-extenor) index))
-          (for/fold ([result result])
-                    ([field-spec (extenorc-field-spec-list extenorc)]
-                     [jndex (in-naturals)])
-            #:break (not (eq? result opaque-flag))
-            (if (and (eq? (car field-spec) 'visible)
-                     (eq? (cdr field-spec) field-symbol))
-                (let ([inner (vector-ref (extenor-content-vector an-extenor)
-                                         index)])
-                  (if (null? (cdr (extenorc-field-spec-list extenorc)))
-                      (if set?
-                          (vector-set (extenor-content-vector an-extenor)
-                                      index new-value-or-fallback)
-                          inner)
-                      (if set?
-                          (vector-set inner jndex new-value-or-fallback)
-                          (vector-ref inner jndex))))
-                result)))))
+              (hash-set (extenor-extenorc-table an-extenor)
+                        extenorc new-value-or-fallback)
+              contents)
+          (let* ([fields (extenorc-field-spec-list extenorc)]
+                 [single-field? (list-length-one? fields)])
+            (for/fold ([result result])
+                      ([field-spec fields]
+                       [index (in-naturals)])
+              #:break (not (eq? result opaque-flag))
+              (if (and (eq? (car field-spec) 'visible)
+                       (eq? (cdr field-spec) field-symbol))
+                  (if set?
+                      (let* ([guard-proc (or (extenorc-guard extenorc)
+                                             (λ(x)x))]
+                             [new-contents
+                              (if single-field?
+                                  (guard-proc (list new-value-or-fallback))
+                                  (guard-proc
+                                   (list-set contents index new-value-or-fallback)))]))
+                      (hash-set (extenor-extenorc-table an-extenor)
+                                extenorc
+                                (if single-field?
+                                    (car new-contents)
+                                    new-contents))
+                      (if single-field?
+                          contents
+                          (list-ref contents index)))
+                  result))))))
   (if (eq? result-tentative opaque-flag)
       (if set?
-          (let* ([new-index (vector-length (extenor-content-vector an-extenor))]
-                 [new-content-vector (apply vector-immutable
-                                            (append (vector->list
-                                                     (extenor-content-vector))
-                                                    (list new-value-or-fallback)))]
-                 [new-table (hash-set (extenor-extenorc-table an-extenor)
-                                      field-symbol
-                                      new-index)]
-                 [st-ctor (extenor-struct-type-constructor an-extenor)])
-            (st-ctor st-ctor new-content-vector new-table))
+          (let ([new-table (hash-set (extenor-extenorc-table an-extenor)
+                                     field-symbol
+                                     new-value-or-fallback)]
+                [st-ctor (extenor-struct-type-constructor an-extenor)])
+            (st-ctor st-ctor new-table))
           (if (procedure? new-value-or-fallback)
               (new-value-or-fallback)
               new-value-or-fallback))
@@ -107,16 +207,26 @@
                            [fallback (λ () (error 'get-extenor-field
                                                   "No value found for key: ~v"
                                                   field-symbol))])
-  (do-extenor-walk #f an-extenor field-symbol fallback))
+  (do-extenor-walk/break #f an-extenor field-symbol fallback))
 
 (define (set-extenor-field an-extenor field-symbol new-value)
-  (do-extenor-walk #t an-extenor field-symbol fallback))
+  (do-extenor-walk/break #t an-extenor field-symbol fallback))
 
 (define (get-extenor-keys an-extenor)
-  (error 'TODO-implement-get-extenor-keys))
+  (for/fold ([result '()])
+            ([extenorc (hash-keys (extenor-extenorc-table an-extenor))])
+    (append (filter-map (λ (fs) (and (eq? 'visible (car fs))
+                                     (cdr fs)))
+                        (extenorc-field-spec-list extenorc))
+            result)))
 
 (define (get-extenor-struct-type-properties an-extenor)
-  (error 'TODO-implement-get-extenor-struct-type-properties))
+  (for/fold ([result '()])
+            ([extenorc (hash-keys (extenor-extenorc-table an-extenor))])
+    (append (map car (extenorc-property-alist extenorc)) result)))
+
+(define (get-extenorc-struct-type-properties an-extenorc)
+  (map car (extenorc-property-alist an-extenorc)))
 
 (define-syntax (define-extenorc stx)
   (define-syntax-class field-spec
@@ -130,24 +240,86 @@
                     #:defaults ([guard-expression #'#f]))
          (~seq #:property prop-expression prop-val-expression))
         ...)
-     (with-syntax ([(accessor ...) (map (λ (fn) (format-id fn "~a-~a" #'name fn))
+     (with-syntax ([(getter ...) (map (λ (fn) (format-id fn "~a-~a" #'name fn))
+                                      (syntax->list #'(fs.field-name ...)))])
+       (with-syntax ([(setter ...) (map (λ (fn) (format-id fn "set-~a-~a" #'name fn))
                                         (syntax->list #'(fs.field-name ...)))])
-       #'(define-values
-           (name accessor ...)
-           (make-extenorc #:name 'name
-                          #:guard guard-expression
-                          #:properties (make-immutable-hash
-                                        (~@ (cons prop-expression
-                                                  prop-val-expression)
-                                            ...))
-                          (cons 'fs.visibility 'fs.field-name) ...)))]))
+         #'(define-values
+             (name predicate getter ... setter ...)
+             (apply values
+                    (flatten
+                     (make-extenorc #:name 'name
+                                    #:guard guard-expression
+                                    #:properties (make-immutable-hash
+                                                  (~@ (cons prop-expression
+                                                            prop-val-expression)
+                                                      ...))
+                                    (cons 'fs.visibility 'fs.field-name) ...))))))]))
 
 (define (make-extenorc
          #:name [name #f]
          #:guard [guard #f]
          #:properties [properties (hash)]
          . field-name-spec)
-  (error 'TODO-implement-make-extenorc))
+  (define this-extenorc
+    (extenorc name
+              field-spec-list
+              (for/list ([(key val) (in-hash properties)])
+                #;(when (not (struct-type-property? key))
+                    (error 'make-extenorc "Not a struct-type-property: ~v" key))
+                (cons key val))
+              guard))
+  (define this-extenorc-predicate
+    (λ (an-extenor)
+      (and (extenor? an-extenor)
+           (hash-has-key? (extenor-extenorc-table an-extenor)
+                          this-extenorc))))
+  (define single-field?
+    (list-length-one? field-name-spec))
+  (define this-extenorc-getters
+    (for/list ([fns field-name-spec]
+               [index (in-naturals)])
+      (λ (an-extenor)
+        (let ([contents
+               (hash-ref (extenor-extenorc-table an-extenor)
+                         this-extenorc
+                         (λ () (error 'extenorc-getter
+                                      "extenorc (~v) not found in extenor (~v)"
+                                      name
+                                      an-extenor)))])
+          (if single-field?
+              contents
+              (list-ref contents index))))))
+  (define this-extenorc-setters
+    (for/list ([fns field-name-spec]
+               [index (in-naturals)])
+      (λ (an-extenor new-val)
+        (when (not (this-extenorc-predicate an-extenor))
+          (error 'extenorc-setter
+                 "extenorc (~v) not found in extenor (~v)"
+                 name
+                 an-extenor))
+        (if single-field?
+            (hash-set (extenor-extenorc-table an-extenor)
+                      this-extenorc
+                      new-val)
+            (let ([contents
+                   (hash-ref (extenor-extenorc-table an-extenor)
+                             this-extenorc)])
+              (hash-set (extenor-extenorc-table an-extenor)
+                        this-extenorc
+                        (list-set contents index new-val)))))))
+  (list
+   this-extenorc
+   this-extenorc-predicate
+   this-extenorc-getters
+   this-extenorc-setters))
 
 (define (make-prop-extenorc prop prop-val)
-  (error 'TODO-implement-make-prop-extenorc))
+  (car (make-extenorc #:properties (hash prop prop-val))))
+
+(define (list-length-one? l)
+  (and (not (null? l))
+       (null? (cdr l))))
+
+(define opaque-flag (gensym))
