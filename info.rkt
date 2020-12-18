2dfa70d9618bacb87fee1e13ae6e9e9c150e732e
William G Hatch <william@hatch.uno>
2020-12-17 17:13:52 -0700

    add basic documentation and package boilerplate

---
+++ info.rkt
@@ -0,0 +1,8 @@
+#lang info
+(define deps '("base"
+               "rackunit-lib"
+               ))
+(define build-deps '("scribble-lib"
+                     "racket-doc"))
+(define scribblings '(("scribblings/extenor.scrbl" () (library))))
+
---
+++ main.rkt
@@ -0,0 +1,3 @@
+#lang racket/base
+(require "private/extenor.rkt")
+(provide (all-from-out "private/extenor.rkt"))
